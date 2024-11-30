64f2f1f3d86ceec44039f5bf347e2613a9f7b89b
Md Refat Hossain <refatibnanwar@gmail.com>
2024-12-01 01:11:16 +0800

    Initial Commit

---
+++ Vapor-crud-ios-app/Vapor_crud_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vapor_crud_ios_appApp.swift
+//  Vapor-crud-ios-app
+//
+//  Created by Md Refat Hossain on 01/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Vapor_crud_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
